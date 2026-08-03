04d2915b9feb9f1a3e215420ea826e5ef5ad8e51
minjun <mrainbeat@gmail.com>
2026-08-03 22:47:56 +0900

    [Fix] 스플래시, 로그인 페이지 ui수정

--- DayBit/src/pages/SplashScreen.jsx
+++ DayBit/src/pages/SplashScreen.jsx
@@ -1,37 +1,24 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import logoImage from "../assets/logos/logo-full.png";
+import LogoFull from "../assets/logos/logo-full.png";
 
 export default function SplashScreen() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // 2.5초 뒤 로그인 페이지로 이동 (뒤로가기 방지 replace: true 적용)
     const timer = setTimeout(() => {
       navigate("/login", { replace: true });
     }, 2500);
-
-    // 컴포넌트 언마운트 시 타이머 종료시킴
     return () => clearTimeout(timer);
   }, [navigate]);
 
   return (
-    // 전체 화면 컨테이너
-    <div className="flex flex-col items-center min-h-screen bg-white animate-fade-in">
-      {/* 상단 여백 : 전체 높이의 34% */}
-      <div className="flex-none" style={{ height: "34%" }}></div>
-
-      {/* 로고 영역 : 전체 높이의 26% */}
-      <div className="flex-1 flex items-center justify-center w-full px-8">
-        <img
-          src={logoImage}
-          alt="Logo"
-          className="w-auto h-auto max-w-[220px] max-h-[220px] object-contain"
-        />
-      </div>
-
-      {/* 하단 여백 : 전체 높이의 40%) */}
-      <div className="flex-none" style={{ height: "40%" }}></div>
+    <div className="relative h-full w-full select-none overflow-hidden bg-[#F6F8FA]">
+      <img
+        src={LogoFull}
+        alt="DAY BIT"
+        className="pointer-events-none absolute left-1/2 top-[286px] w-[160px] -translate-x-1/2 object-contain"
+      />
     </div>
   );
 }
