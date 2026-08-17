60986d57a6919eb3882eb1b7fdc2f52682d51ed9
kimen <kmkang0619@gmail.com>
2026-08-18 05:06:12 +0900

    feat: 일기작성 튜토리얼에 전환 애니메이션 적용

--- DayBit/src/pages/Diary/DiaryPage.jsx
+++ DayBit/src/pages/Diary/DiaryPage.jsx
@@ -443,7 +443,7 @@ export default function DiaryPage() {
                   type="button"
                   onClick={handleComplete}
                   disabled={!hasUserWritten && tutorialStep !== 2}
-                  className="w-[118px] h-[48px] rounded-[12px] px-[26px] text-[18px] font-semibold tracking-[-0.18px] bg-grey-70 text-grey-0 disabled:bg-grey-20 disabled:text-grey-0 disabled:cursor-not-allowed transition-colors duration-700 delay-200 ease-in-out whitespace-nowrap"
+                  className="w-[118px] h-[48px] rounded-[12px] px-[26px] text-[18px] font-semibold tracking-[-0.18px] bg-grey-70 text-grey-0 disabled:bg-grey-20 disabled:text-grey-0 disabled:cursor-not-allowed transition-colors duration-200 ease-out whitespace-nowrap"
                 >
                   작성 완료
                 </button>
